4d662f8d6af38e0a94dbc967a8b50aee5a05def3
sebastian <sebastian.iwanczyszyn@gmail.com>
2025-02-02 21:52:07 +0100

    Add units & Improve CI

---
+++ test/unit/channel/factory/in-memory-channel.factory.spec.ts
@@ -0,0 +1,26 @@
+import {
+  ChannelConfig,
+  InMemoryChannelConfig,
+  InMemoryChannelFactory,
+  InvalidChannelConfigException,
+} from '../../../../src';
+import { InMemoryChannel } from '../../../../src/channel/in-memory.channel';
+
+describe('InMemoryChannelFactory', () => {
+  let factory: InMemoryChannelFactory;
+
+  beforeEach(() => {
+    factory = new InMemoryChannelFactory();
+  });
+
+  test('should create an InMemoryChannel when given a valid InMemoryChannelConfig', () => {
+    const config = new InMemoryChannelConfig({name: 'testInMemoryChannelConfig'});
+    const channel = factory.create(config);
+    expect(channel).toBeInstanceOf(InMemoryChannel);
+  });
+
+  test('should throw an exception when given an invalid config type', () => {
+    const invalidConfig = {} as ChannelConfig;
+    expect(() => factory.create(invalidConfig)).toThrow(InvalidChannelConfigException);
+  });
+});
