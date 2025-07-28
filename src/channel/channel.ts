56b613d11dea9c7eab05dd3c6a75e36f8a2afefd
sebastian <sebastian.iwanczyszyn@gmail.com>
2025-07-28 21:54:36 +0200

    fix: improve and fix logger for async consumer

--- src/channel/channel.ts
+++ src/channel/channel.ts
@@ -3,5 +3,5 @@ import { ChannelConfig } from '../config';
 export class Channel<T extends ChannelConfig> {
   constructor(public readonly config: T) {}
 
-  async onChannelDestroy(): Promise<void> {};
+  async onChannelDestroy(): Promise<void> {}
 }
