ad9fc19e91f2778132e85d20856bc7c0f7369489
Sebastian Iwanczyszyn <57344470+Sebastian-Iwanczyszyn@users.noreply.github.com>
2025-09-06 00:11:40 +0200

    feat: BREAKING CHANGE drop support for amqp config (#30)

--- src/config.ts
+++ src/config.ts
@@ -71,49 +71,6 @@ export class ChannelConfig {
   }
 }
 
-/**
- * @deprecated Will be removed in version 3.x - use RmqChannelConfig from @nestjstools/messaging-rabbitmq-extension version 2.11+
- */
-export class AmqpChannelConfig extends ChannelConfig {
-  public readonly connectionUri: string;
-  public readonly exchangeName: string;
-  public readonly exchangeType: ExchangeType;
-  public readonly queue: string;
-  public readonly bindingKeys?: string[];
-  public readonly autoCreate?: boolean;
-  public readonly deadLetterQueueFeature?: boolean;
-
-  constructor({
-    name,
-    connectionUri,
-    exchangeName,
-    exchangeType,
-    queue,
-    enableConsumer,
-    bindingKeys,
-    autoCreate,
-    deadLetterQueueFeature,
-    avoidErrorsForNotExistedHandlers,
-    middlewares,
-    normalizer,
-  }: AmqpChannelConfig) {
-    super(
-      name,
-      avoidErrorsForNotExistedHandlers,
-      middlewares,
-      enableConsumer,
-      normalizer,
-    );
-    this.connectionUri = connectionUri;
-    this.exchangeName = exchangeName;
-    this.exchangeType = exchangeType;
-    this.queue = queue;
-    this.bindingKeys = bindingKeys;
-    this.autoCreate = autoCreate ?? true;
-    this.deadLetterQueueFeature = deadLetterQueueFeature ?? false;
-  }
-}
-
 export class InMemoryChannelConfig extends ChannelConfig {
   constructor({
     name,
