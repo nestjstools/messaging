9eba292a881734e38aada64886dd9d1b87d64fb1
sebastian <sebastian.iwanczyszyn@gmail.com>
2026-03-15 01:15:07 +0100

    chore: improve ci + codebase

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,6 +28,8 @@ export default tseslint.config(
       sourceType: 'module',
     },
     rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
--- packages/messaging/src/consumer/consumer-message.ts
+++ packages/messaging/src/consumer/consumer-message.ts
@@ -2,7 +2,7 @@ export class ConsumerMessage {
   constructor(
     public readonly message: object | string,
     public readonly routingKey: string,
-    public readonly metadata: {} = {},
+    public readonly metadata: Record<string, unknown> = {},
   ) {
     this.message = message;
     this.routingKey = routingKey;
