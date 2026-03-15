f759dcbe593ed859bbe47882b16b1536597f1926
sebastian <sebastian.iwanczyszyn@gmail.com>
2026-03-15 01:23:31 +0100

    chore: improve ci + codebase

--- packages/messaging/src/consumer/consumer-message.ts
+++ packages/messaging/src/consumer/consumer-message.ts
@@ -2,7 +2,7 @@ export class ConsumerMessage {
   constructor(
     public readonly message: object | string,
     public readonly routingKey: string,
-    public readonly metadata: Record<string, unknown> = {},
+    public readonly metadata: Record<string, string|number> = {},
   ) {
     this.message = message;
     this.routingKey = routingKey;
