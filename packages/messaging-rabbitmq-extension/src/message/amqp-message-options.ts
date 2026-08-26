ea1f8aaec9fa6c68adb56f89bb65bad6b29a2359
sebastian <sebastian.iwanczyszyn@gmail.com>
2026-08-26 20:21:41 +0200

    [messaging] feat: new version of messaging

--- packages/messaging-rabbitmq-extension/src/message/amqp-message-options.ts
+++ packages/messaging-rabbitmq-extension/src/message/amqp-message-options.ts
@@ -1,35 +1,12 @@
-interface AmqpMessageOptionsProps {
-  exchangeName?: string;
-  routingKey?: string;
-  headers?: { [key: string]: string };
-}
+import { MessageOptions } from '@nestjstools/messaging';
+import { Middleware } from '@nestjstools/messaging';
 
-export class AmqpMessageOptions {
-  public readonly exchangeName?: string;
-  public readonly routingKey?: string;
-  public readonly headers: { [key: string]: string };
-
-  constructor(props?: AmqpMessageOptionsProps);
-  /** @deprecated Use the object-form constructor. This overload will be removed in the next major version. */
+export class AmqpMessageOptions implements MessageOptions {
   constructor(
-    exchangeName?: string,
-    routingKey?: string,
-    headers?: { [key: string]: string },
-  );
-  constructor(
-    propsOrExchangeName?: AmqpMessageOptionsProps | string,
-    routingKey?: string,
-    headers: { [key: string]: string } = {},
-  ) {
-    if (typeof propsOrExchangeName === 'string' || arguments.length > 1) {
-      this.exchangeName = propsOrExchangeName as string | undefined;
-      this.routingKey = routingKey;
-      this.headers = headers;
-      return;
-    }
-
-    this.exchangeName = propsOrExchangeName?.exchangeName;
-    this.routingKey = propsOrExchangeName?.routingKey;
-    this.headers = propsOrExchangeName?.headers ?? {};
-  }
+    public readonly exchangeName: string | undefined = undefined,
+    public readonly routingKey: string | undefined = undefined,
+    public readonly middlewares: Middleware[] = [],
+    public readonly headers: { [key: string]: string } = {},
+    public readonly avoidErrorsWhenNotExistedHandler: boolean = undefined,
+  ) {}
 }
