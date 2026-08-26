815ca49b042e7ccf78647520314a0eb65718d70d
sebastian <sebastian.iwanczyszyn@gmail.com>
2026-08-26 22:39:10 +0200

    [messaging] feat: improve message options / bump libs

---
+++ packages/messaging-nats-extension/src/message/nats-jet-stream-message-options.ts
@@ -0,0 +1,24 @@
+import type { JetStreamPublishOptions } from '@nats-io/jetstream';
+import { NatsMessageOptions } from './nats-message-options';
+
+export interface NatsJetStreamMessageOptionsProps extends Pick<
+  Partial<JetStreamPublishOptions>,
+  'retries' | 'schedule' | 'timeout' | 'ttl'
+> {
+  headers?: Record<string, string>;
+}
+
+export class NatsJetStreamMessageOptions extends NatsMessageOptions {
+  public readonly ttl?: string;
+  public readonly retries?: number;
+  public readonly schedule?: JetStreamPublishOptions['schedule'];
+  public readonly timeout?: number;
+
+  constructor(props: NatsJetStreamMessageOptionsProps = {}) {
+    super(props.headers);
+    this.ttl = props.ttl;
+    this.retries = props.retries;
+    this.schedule = props.schedule;
+    this.timeout = props.timeout;
+  }
+}
